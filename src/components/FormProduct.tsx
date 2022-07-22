c419c35c00f31244c6b9a4a263366f2ab91c755a
Mario Carrillo <mario.carrillo@clearcheck.us>
2022-07-22 17:31:28 -0500

    fix

--- src/components/FormProduct.tsx
+++ src/components/FormProduct.tsx
@@ -178,7 +178,7 @@ export default function FormProduct({ setOpen, setAlert, product }: any) {
                         autoComplete="description"
                         rows={3}
                         className={`form-textarea
-                     mt-1 block w-full mt-1 
+                     mt-1 
                      focus:ring-indigo-500 
                      focus:border-indigo-500 block w-full 
                      shadow-sm sm:text-sm 
