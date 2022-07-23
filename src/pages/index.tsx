c0562e21926e192c335ca3ff16ca4248ac1952ab
Mario Carrillo <mario.carrillo@clearcheck.us>
2022-07-23 13:16:15 -0500

    fixes for deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -90,11 +90,9 @@ const Home = () => {
                         <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
                            <div className="px-5 pt-4 flex items-center justify-between">
                               <div>
-                                 <Image
+                                 <img
                                     src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                                     className="h-8 w-auto"
-                                    width={32}
-                                    height={32}
                                     alt=""
                                  />
                               </div>
@@ -160,10 +158,8 @@ const Home = () => {
             </div>
          </div>
          <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-            <Image
+            <img
                src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-               width={32}
-               height={224}
                className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
                alt=""
             />
